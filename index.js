251da0db7ec310370e11c7ed7486a563e9b920ab
nasriderowsaney <nasriderowsaney@gmail.com>
2026-01-22 13:22:09 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+// findMatching
+function findMatching(drivers, name) {
+  return drivers.filter(driver =>
+    driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+// fuzzyMatch
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver =>
+    driver.toLowerCase().startsWith(letters.toLowerCase())
+  );
+}
+
+// matchName
+function matchName(drivers, name) {
+  return drivers.filter(driver =>
+    driver.name === name
+  );
+}
